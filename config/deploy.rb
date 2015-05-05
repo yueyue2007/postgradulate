b434fddfe7ce94128e1f66df8ddc59f16efe2304
duhang zhong <zdh@centos6.localdomain>
2015-05-05 11:17:14 +0800

    added edit and delete

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
+set :application, 'postgradulate'
 set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/vagrant/www/postgradulate/postgradulate'
 
 # Default value for :scm is :git
 # set :scm, :git
