715a55775149587a29127b516d960f8657abae15
duhang zhong <zdh@centos6.localdomain>
2015-05-07 15:31:23 +0800

    database.yml and secrets.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,10 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-SSHKit.config.command_map[:rake] = "bundle exec rake"
+#ssh-add #need this to make key-forwarding work??
+
+#SSHKit.config.command_map[:rake] = "bundle exec rake"
+
 set :application, 'postgradulate'
 set :repo_url, 'git@github.com:yueyue2007/postgradulate.git' 
 
@@ -24,7 +27,7 @@ set :deploy_to, '/home/vagrant/www/postgradulate/postgradulate'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
